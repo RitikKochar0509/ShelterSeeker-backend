20a1df445521509ebd849e3575270e45ce4066ae
Ritik Kochar <ritikkochar2@gmail.com>
2023-09-28 19:18:12 +0530

    integrate mongodb and made some changes

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express');
 
 const app =express();
 const mongoose = require("mongoose");
-
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
+const PORT = process.env.PORT;
+mongoose.connect(process.env.DATABASE_URL,
+{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 
 const db = mongoose.connection
 db.on('error',(error)=>console.log(error));
@@ -15,4 +19,4 @@ app.use(express.json());
 const properyRoutes = require("./routes/propertyRoutes");
 app.use('/api',properyRoutes);
 
-app.listen(3000,()=>console.log("server started"));
+app.listen(PORT,()=>console.log("server started"));
