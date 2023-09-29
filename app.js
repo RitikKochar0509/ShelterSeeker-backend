49801cf0159ebd76c4dff48436ac8f53838c9f55
Ritik Kochar <ritikkochar2@gmail.com>
2023-09-29 16:21:30 +0530

    fix cors error

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-require('dotenv').config();
 const express = require('express');
-
-const app =express();
 const mongoose = require("mongoose");
+const cors = require('cors'); // Import the cors middleware
+require('dotenv').config();
+
+const app = express();
+
 const PORT = process.env.PORT || 8000;
-mongoose.connect('mongodb+srv://ritikkochar2:T0kFC4oBLqwumJFX@shelterseeker.7kjxe02.mongodb.net/?retryWrites=true&w=majority',
-{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 
-const db = mongoose.connection
-db.on('error',(error)=>console.log(error));
-db.once('open',()=>console.log("databaseconntected"));
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log("database connected"));
+
+// Use the cors middleware to enable CORS for all routes
+app.use(cors());
 
 app.use(express.json());
 
 const properyRoutes = require("./routes/propertyRoutes");
-app.use('/api',properyRoutes); 
+app.use('/api', properyRoutes);
 
-app.listen(PORT,()=>console.log("server started"));
+app.listen(PORT, () => console.log("server started"));
